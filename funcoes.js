bcdaee94b4e219fa435fd7c353659fa22cf3a12b
LuisGuilhermeCipriano <59087567+LuisGuilhermeCipriano@users.noreply.github.com>
2020-03-26 22:02:12 -0300

    Termino

--- funcoes.js
+++ funcoes.js
@@ -1,1 +1,17 @@
-alert("Exemplo alerta javascript");
+//Atividade
+
+var x = 0;
+var y = 10;
+var brincadeira = "Seu computador vai desligar em ";
+
+while (x<=y)
+{
+    alert(brincadeira + x);
+    x++;
+}
+
+if (x=10)
+{
+    alert("Brincadeira kk");
+}
+
